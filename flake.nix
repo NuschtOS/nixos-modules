7228139f4323702708a92aa5f902e61575e3e4d5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-12-22 21:12:38 +0100

    Remove extra directories

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-  outputs = _:
+  inputs = {
+    lib.url = "github:nix-community/nixpkgs.lib";
+  };
+
+  outputs = { lib, ... }:
     let
-      dirs = dir: builtins.attrNames (builtins.readDir (./. + "/${dir}"));
-      importToAttr = dir: inputAttr: builtins.listToAttrs (map (p: { name = p; value = import (./. + "/${dir}/${p}") inputAttr; }) (dirs dir));
+      ls = dir: builtins.attrNames (builtins.readDir (./. + "/${dir}"));
+      importToAttr = dir: inputAttr: lib.listToAttrs (map (file: { name = lib.removeSuffix ".nix" file; value = import (./. + "/${dir}/${file}.nix") inputAttr; }) (ls dir));
     in
     {
       lib = inputAttr: importToAttr "lib" inputAttr;
       nixosModules = inputAttr: importToAttr "modules" inputAttr;
       nixosModule = _: {
-        imports = (dir: map (p: ./. + "/${dir}/${p}") (dirs dir)) "modules";
+        imports = (dir: map (file: ./. + "/${dir}/${file}") (ls dir)) "modules";
       };
     };
 }
