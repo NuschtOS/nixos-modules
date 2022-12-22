1f173f875c2e23c9b0bd0a7d148b9bc79cd46b4f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-12-22 21:58:07 +0100

    nginx: only open ports when nginx is enabled

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -2,12 +2,12 @@
 
 {
   options.services.nginx.openFirewall = lib.mkOption {
-    description = lib.mdDoc "Wether to open the firewall port for the http (80) and https (443) default ports.";
-    default = false;
     type = lib.types.bool;
+    default = false;
+    description = lib.mdDoc "Wether to open the firewall port for the http (80) and https (443) default ports.";
   };
 
-  config = lib.mkIf config.services.nginx.openFirewall {
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+  config = lib.mkIf config.services.nginx.enable {
+    networking.firewall.allowedTCPPorts = lib.mkIf config.services.nginx.openFirewall [ 80 443 ];
   };
 }
