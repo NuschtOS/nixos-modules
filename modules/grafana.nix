32d041f3239b55d3cfbf39cc496199e5e86ff3d0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-12-23 05:53:53 +0100

    Add grafana

---
+++ modules/grafana.nix
@@ -0,0 +1,30 @@
+{ config, lib, ...}:
+
+let
+  cfg = config.services.grafana;
+in
+{
+  options.services.grafana.opinionatedDefaults = lib.mkOption {
+    type = lib.types.bool;
+    default = config.opinionatedDefaults;
+    description = lib.mdDoc "Wether to enable set opinionated default settings.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.grafana.settings = lib.mkIf cfg.opinionatedDefaults (lib.mapAttrsRecursive (path: value: lib.mkDefault value) {
+      analytics = {
+        check_for_updates = false;
+        reporting_enabled = false;
+      };
+      security = {
+        cookie_secure = true;
+        content_security_policy = true;
+      };
+      server = {
+        enable_gzip = true;
+        http_addr = "127.0.0.1";
+        root_url = "https://${cfg.settings.server.domain}";
+      };
+    });
+  };
+}
