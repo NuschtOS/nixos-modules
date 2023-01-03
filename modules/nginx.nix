3ebd733623b6c21fbfa36fee8ca569a12d9d78c2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-01-03 02:05:46 +0100

    nginx: add option for subparts

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -5,17 +5,13 @@ let
 in
 {
   options.services.nginx = {
-    generateDhparams = lib.mkOption {
-      type = lib.types.bool;
-      default = config.opinionatedDefaults;
-      description = lib.mdDoc "Wether to generate more secure, 2048 bits dhparams replacing the default 1024 bits.";
-    };
+    allRecommended = libS.mkOpinionatedOption "all recommended options";
 
-    openFirewall = lib.mkOption {
-      type = lib.types.bool;
-      default = config.opinionatedDefaults;
-      description = lib.mdDoc "Wether to open the firewall port for the http (80) and https (443) default ports.";
-    };
+    generateDhparams = libS.mkOpinionatedOption "generate more secure, 2048 bits dhparams replacing the default 1024 bits";
+
+    openFirewall = libS.mkOpinionatedOption "open the firewall port for the http (80) and https (443) default ports";
+
+    setResolverAddresses = libS.mkOpinionatedOption "set resolver address to environment.nameservers";
   };
 
   config = lib.mkIf cfg.enable {
@@ -23,15 +19,6 @@ in
 
     services.nginx = lib.mkMerge [
       {
-        sslDhparam = config.security.dhparams.params.nginx.path;
-      }
-
-      (lib.mkIf config.opinionatedDefaults (libS.modules.mkRecursiveDefault {
-        recommendedBrotliSettings = true;
-        recommendedGzipSettings = true;
-        recommendedOptimisation = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
         resolver.addresses = let
           isIPv6 = addr: builtins.match ".*:.*:.*" addr != null;
           escapeIPv6 = addr: if isIPv6 addr then
@@ -39,11 +26,20 @@ in
           else
             addr;
         in
-          lib.optionals (config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
+          lib.optionals (cfg.setResolverAddresses && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
+        sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
+      }
+
+      (lib.mkIf cfg.allRecommended (libS.modules.mkRecursiveDefault {
+        recommendedBrotliSettings = true;
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
       }))
     ];
 
-    security.dhparams = {
+    security.dhparams = lib.mkIf cfg.generateDhparams {
       enable = cfg.generateDhparams;
       params.nginx = { };
     };
