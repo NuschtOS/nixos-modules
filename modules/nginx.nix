aafa533bd130f9a235a3bde3e208cfb171db909b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-01-10 00:12:10 +0100

    nginx: rename two settings

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,4 +1,4 @@
-{ config, lib, libS, ... }:
+{ config, lib, libS, pkgs, ... }:
 
 let
   cfg = config.services.nginx;
@@ -7,20 +7,20 @@ in
   options.services.nginx = {
     allRecommended = libS.mkOpinionatedOption "all recommended options";
 
-    keepLessLog = libS.mkOpinionatedOption "keep logs only for 7 days and rotate them daily";
-
     generateDhparams = libS.mkOpinionatedOption "generate more secure, 2048 bits dhparams replacing the default 1024 bits";
 
     openFirewall = libS.mkOpinionatedOption "open the firewall port for the http (80) and https (443) default ports";
 
-    setResolverAddresses = libS.mkOpinionatedOption "set resolver address to environment.nameservers";
+    resolverAddrFromNameserver = libS.mkOpinionatedOption "set resolver address to environment.nameservers";
+
+    rotateLogsFaster = libS.mkOpinionatedOption "keep logs only for 7 days and rotate them daily";
   };
 
   config = lib.mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ 80 443 ];
 
     services = {
-      logrotate.settings.nginx = lib.mkIf cfg.keepLessLog {
+      logrotate.settings.nginx = lib.mkIf cfg.rotateLogsFaster {
         frequency = "daily";
         rotate = 7;
       };
@@ -34,7 +34,7 @@ in
             else
               addr;
           in
-            lib.optionals (cfg.setResolverAddresses && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
+            lib.optionals (cfg.resolverAddrFromNameserver && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
           sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
         }
 
