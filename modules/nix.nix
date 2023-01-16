dd49629a0671b90d482de50aadfbceea91fe8bcb
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-01-17 00:34:56 +0100

    nix: add settings to cleanup legacy, none flake things

---
+++ modules/nix.nix
@@ -0,0 +1,23 @@
+{ config, lib, libS, ... }:
+
+{
+  options.nix = {
+    deleteChannels = lib.mkEnableOption "" // { description = "Wether to delete all channels on a system switch."; };
+
+    deleteUserProfiles = lib.mkEnableOption "" // { description = "Wether to delete all channels on a system switch."; };
+  };
+
+  config = {
+    system.userActivationScripts = {
+      deleteChannels = lib.mkIf config.nix.deleteChannels ''
+        echo "Deleting all channels..."
+        rm -rf /root/.nix-channels /home/*/.nix-channels /nix/var/nix/profiles/per-user/*/channels*
+      '';
+
+      deleteUserProfiles = lib.mkIf config.nix.deleteUserProfiles ''
+        echo "Deleting all user profiles..."
+        rm -rf /root/.nix-profile /home/*/.nix-profile /nix/var/nix/profiles/per-user/*/profile*
+      '';
+    };
+  };
+}
