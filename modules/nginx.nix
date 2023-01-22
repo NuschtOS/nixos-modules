7b3ecd1d5a08ecca70fcb320823004ad0f0f7a64
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-01-22 19:57:24 +0100

    nginx: add option for default 404 server

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -7,6 +7,25 @@ in
   options.services.nginx = {
     allRecommended = libS.mkOpinionatedOption "all recommended options";
 
+    default404Server = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = lib.mdDoc ''
+          Wether to add a default server which always responds with 404.
+          This is useful when using a wildcard cname with a wildcard certitificate to not return the first server entry in the config on unknown subdomains
+          or to do the same for an old and not fully removed domain.
+        '';
+      };
+
+      acmeHost = lib.mkOption {
+        type = lib.types.str;
+        description = lib.mdDoc ''
+          The acme host to use for the default 404 server.
+        '';
+      };
+    };
+
     generateDhparams = libS.mkOpinionatedOption "generate more secure, 2048 bits dhparams replacing the default 1024 bits";
 
     openFirewall = libS.mkOpinionatedOption "open the firewall port for the http (80) and https (443) default ports";
@@ -38,6 +57,17 @@ in
             in
             lib.optionals (cfg.resolverAddrFromNameserver && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
           sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
+
+          virtualHosts = lib.mkIf cfg.default404Server.enable {
+            "_" = {
+              default = true;
+              forceSSL = lib.mkDefault true;
+              useACMEHost = cfg.default404Server.acmeHost;
+              extraConfig = ''
+                return 404;
+              '';
+            };
+          };
         }
 
         (lib.mkIf cfg.allRecommended (libS.modules.mkRecursiveDefault {
