d244f389e5f57d429e3cc23fe05fc48497f96ebd
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-04-04 00:05:25 +0200

    portunus: move patches to here

--- modules/portunus.nix
+++ modules/portunus.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.services.portunus;
@@ -37,6 +37,25 @@ in
       ${cfg.internalIp4} = [ cfg.domain ];
       ${cfg.internalIp6} = [ cfg.domain ];
     };
+
+    nixpkgs.overlays = with pkgs; [
+      (final: prev: {
+        portunus = prev.portunus.overrideAttrs ({ patches ? [ ], ... }: {
+          patches = patches ++ [
+            # allow editing members of groups
+            (fetchpatch {
+              url = "https://github.com/majewsky/portunus/commit/70ebf6abf944f3b5064169a2ac9d5f2ddcc7b58c.patch";
+              sha256 = "sha256-fZzOuJ6K1NXJHWvOfSIU5FAfL0dVK7b7dhhtb6yuCGE=";
+            })
+            # fix creating new groups with members
+            (fetchpatch {
+              url = "https://github.com/majewsky/portunus/commit/d4f0ca61fde0c9524a4230cfc3be2e8f51cb2a89.patch";
+              sha256 = "sha256-9VPIn5JeWqrO4dITt9nHUf5sUfLb9w3DNZBArybjuLs=";
+            })
+          ];
+        });
+      })
+    ];
 
     security.ldap = lib.mkIf cfg.ldapPreset {
       domainName = cfg.domain;
