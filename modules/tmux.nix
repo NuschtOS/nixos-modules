9f88c627a99c1b5c6aa318cf99976868a111bbdb
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-04-12 19:52:59 +0200

    tmux: add

---
+++ modules/tmux.nix
@@ -0,0 +1,27 @@
+{ config, lib, libS, ... }:
+
+let
+  cfg = config.programs.tmux;
+in
+{
+  options = {
+    programs.tmux.allRecommended = libS.mkOpinionatedOption "set recommended tmux settings";
+  };
+
+  config = lib.mkIf cfg.allRecommended {
+    aggressiveResize = true;
+    baseIndex = 1;
+    clock24 = true;
+    escape-time = 100;
+    terminal = "xterm-256color";
+    extraConfig = ''
+      # focus events enabled for terminals that support them
+      set -g focus-events on
+
+      # open new tab in PWD
+      bind '"' split-window -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+    '';
+  };
+}
