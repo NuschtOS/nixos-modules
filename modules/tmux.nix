97cd2ff803cdbefdcc5523b47eb372dbd4e3957f
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-04-13 10:53:43 +0200

    tmux: fix option location

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -9,19 +9,21 @@ in
   };
 
   config = lib.mkIf cfg.allRecommended {
-    aggressiveResize = true;
-    baseIndex = 1;
-    clock24 = true;
-    escape-time = 100;
-    terminal = "xterm-256color";
-    extraConfig = ''
-      # focus events enabled for terminals that support them
-      set -g focus-events on
+    programs.tmux = {
+      aggressiveResize = true;
+      baseIndex = 1;
+      clock24 = true;
+      escape-time = 100;
+      terminal = "xterm-256color";
+      extraConfig = ''
+        # focus events enabled for terminals that support them
+        set -g focus-events on
 
-      # open new tab in PWD
-      bind '"' split-window -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
-    '';
+        # open new tab in PWD
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+      '';
+    };
   };
 }
