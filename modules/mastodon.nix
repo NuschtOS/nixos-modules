0000000ae744a30ef7a0d4ff39d5e6ae48b0c7d2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-06-28 00:53:04 +0200

    mastodon: add userFilterGroup option

--- modules/mastodon.nix
+++ modules/mastodon.nix
@@ -1,17 +1,23 @@
 { config, lib, ... }:
 
 let
-  cfg = config.services.mastodon;
+  cfg = config.services.mastodon.ldap;
   inherit (config.security) ldap;
 in
 {
   options = {
     services.mastodon.ldap = {
       enable = lib.mkEnableOption (lib.mdDoc "login only via LDAP");
+
+      userFilterGroup = lib.mkOption {
+        type = with lib.types; nullOr str;
+        default = null;
+        description = lib.mdDoc "Restrict logins to users in this group";
+      };
     };
   };
 
-  config = lib.mkIf cfg.ldap.enable {
+  config = lib.mkIf cfg.enable {
     services.mastodon.extraConfig = {
       LDAP_ENABLED = "true";
       LDAP_BASE = ldap.userBaseDN;
@@ -19,11 +25,11 @@ in
       LDAP_HOST = ldap.domainName;
       LDAP_METHOD = "simple_tls";
       LDAP_PORT = toString ldap.port;
-      # TODO: use security.ldap.userFilter
-      LDAP_SEARCH_FILTER = "(&${ldap.groupFilter "mastodon"}(|(%{uid}=%{email})(%{mail}=%{email})))";
       LDAP_UID = ldap.userField;
       # convert .,- (space) in LDAP usernames to underscore, otherwise those users cannot log in
       LDAP_UID_CONVERSION_ENABLED = "true";
+    } // lib.optionalAttrs (cfg.userFilterGroup != null) {
+      LDAP_SEARCH_FILTER = "(&${ldap.groupFilter cfg.userFilterGroup}(|(%{uid}=%{email})(%{mail}=%{email})))";
     };
   };
 }
