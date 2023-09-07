4f9b2df8437841ad2d86657d3d6324569e653737
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-09-08 01:14:17 +0200

    nginx: add quic, tcp fast open, other

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,11 +1,11 @@
-{ config, lib, libS, ... }:
+{ config, lib, libS, pkgs, ... }:
 
 let
   cfg = config.services.nginx;
 in
 {
   options.services.nginx = {
-    allRecommended = libS.mkOpinionatedOption "set all recommended options";
+    allCompression = libS.mkOpinionatedOption "set all recommended compression options";
 
     default404Server = {
       enable = lib.mkOption {
@@ -30,11 +30,21 @@ in
 
     openFirewall = libS.mkOpinionatedOption "open the firewall port for the http (80) and https (443) default ports";
 
+    quic = {
+      enable = lib.mkEnableOption (lib.mdDoc "quic support in nginx");
+
+      bpf = libS.mkOpinionatedOption "configure nginx' bpf support which routes quic packets from the same source to the same worker";
+    };
+
+    recommendedDefaults = libS.mkOpinionatedOption "set recommended performance options not grouped into other settings";
+
     resolverAddrFromNameserver = libS.mkOpinionatedOption "set resolver address to environment.nameservers";
 
     rotateLogsFaster = libS.mkOpinionatedOption "keep logs only for 7 days and rotate them daily";
 
-    setHSTSHeader = libS.mkOpinionatedOption "to add the HSTS header to all virtual hosts";
+    setHSTSHeader = libS.mkOpinionatedOption "add the HSTS header to all virtual hosts";
+
+    tcpFastOpen = libS.mkOpinionatedOption "enable tcp fast open";
   };
 
   config = lib.mkIf cfg.enable {
@@ -48,7 +58,14 @@ in
 
       nginx = lib.mkMerge [
         {
-          commonHttpConfig = lib.mkIf cfg.cfg.recommendedZstdSettings /* nginx */ ''
+          appendConfig = lib.mkIf cfg.recommendedDefaults /* nginx */ ''
+            worker_processes auto;
+            worker_cpu_affinity auto;
+          '';
+
+          commonHttpConfig = lib.mkIf cfg.recommendedDefaults /* nginx */ ''
+            error_log syslog:server=unix:/dev/log;
+          '' + lib.mkIf cfg.recommendedZstdSettings /* nginx */ ''
             # TODO: upstream this?
             zstd_types application/x-nix-archive;
           '';
@@ -69,19 +86,61 @@ in
             lib.optionals (cfg.resolverAddrFromNameserver && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
           sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
 
-          virtualHosts = lib.mkIf cfg.default404Server.enable {
-            "_" = {
-              default = true;
-              forceSSL = lib.mkDefault true;
-              useACMEHost = cfg.default404Server.acmeHost;
-              extraConfig = /* nginx */ ''
-                return 404;
-              '';
-            };
-          };
+          virtualHosts = lib.mkMerge [
+            (lib.mkIf cfg.default404Server.enable {
+              "_" = {
+                default = true;
+                forceSSL = lib.mkDefault true;
+                useACMEHost = cfg.default404Server.acmeHost;
+                extraConfig = /* nginx */ ''
+                  return 404;
+                '';
+              };
+            })
+
+            (lib.mkIf cfg.recommendedDefaults {
+              "_" = {
+                kTLS = true;
+                reuseport = true;
+              };
+            })
+
+            (lib.mkIf cfg.tcpFastOpen (let
+              extraParameters = [
+                # net.core.somaxconn is set to 4096
+                # see https://www.nginx.com/blog/tuning-nginx/#:~:text=to%20a%20value-,greater%20than%20512,-%2C%20change%20the%20backlog
+                "backlog=1024"
+
+                "deferred"
+                "fastopen=256" # requires nginx to be compiled with -DTCP_FASTOPEN=23
+              ];
+            in {
+              "_".listen = lib.mkDefault [
+                { addr = "[::]"; port = 80; inherit extraParameters; }
+                { addr = "[::]"; port = 443; ssl = true; inherit extraParameters; }
+              ];
+            }))
+          ];
         }
 
-        (lib.mkIf cfg.allRecommended (libS.modules.mkRecursiveDefault {
+        (lib.mkIf cfg.quic.enable {
+          appendConfig = lib.mkIf cfg.quic.bpf /* nginx */ ''
+            quic_bpf on;
+          '';
+
+          commonHttpConfig = lib.mkf cfg.quic.enable /* nginx */''
+            quic_retry on;
+          '';
+
+          package = pkgs.nginxQuic; # based on pkgs.nginxMainline
+
+          virtualHosts."_" = {
+            quic = true;
+            reuseport = true;
+          };
+        })
+
+        (lib.mkIf cfg.allCompression (libS.modules.mkRecursiveDefault {
           recommendedBrotliSettings = true;
           recommendedGzipSettings = true;
           recommendedOptimisation = true;
@@ -95,6 +154,13 @@ in
     security.dhparams = lib.mkIf cfg.generateDhparams {
       enable = cfg.generateDhparams;
       params.nginx = { };
+    };
+
+    systemd.services.nginx.serviceConfig = lib.mkIf cfg.quic.bpf {
+      # NOTE: CAP_BPF is included in CAP_SYS_ADMIN but it is not enough alone
+      AmbientCapabilities = [ "CAP_BPF" "CAP_NET_ADMIN" "CAP_SYS_ADMIN" ];
+      CapabilityBoundingSet = [ "CAP_BPF" "CAP_NET_ADMIN" "CAP_SYS_ADMIN" ];
+      SystemCallFilter = [ "bpf" ];
     };
   };
 }
