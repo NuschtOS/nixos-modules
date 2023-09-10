7adbab67fd91b05095b46221b3772b1e8cd01d0b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-09-10 13:28:57 +0200

    nginx: reduce mkMerge usage

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -56,14 +56,16 @@ in
     networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ 80 443 ];
 
     nixpkgs.overlays = lib.mkIf cfg.tcpFastOpen [
-      (final: prev: let
-        configureFlags = [ "-DTCP_FASTOPEN=23" ];
-      in {
-        nginx = prev.nginx.override { inherit configureFlags; };
-        nginxQuic = prev.nginxQuic.override { inherit configureFlags; };
-        nginxStable = prev.nginxStable.override { inherit configureFlags; };
-        nginxMainline = prev.nginxMainline.override { inherit configureFlags; };
-      })
+      (final: prev:
+        let
+          configureFlags = [ "-DTCP_FASTOPEN=23" ];
+        in
+        {
+          nginx = prev.nginx.override { inherit configureFlags; };
+          nginxQuic = prev.nginxQuic.override { inherit configureFlags; };
+          nginxStable = prev.nginxStable.override { inherit configureFlags; };
+          nginxMainline = prev.nginxMainline.override { inherit configureFlags; };
+        })
     ];
 
     services = {
@@ -72,101 +74,82 @@ in
         rotate = 7;
       };
 
-      nginx = lib.mkMerge [
-        (lib.mkIf cfg.recommendedDefaults {
-          appendConfig = /* nginx */ ''
-            worker_processes auto;
-            worker_cpu_affinity auto;
-          '';
+      # NOTE: do not use mkMerge here to prevent infinite recursions
+      nginx = {
+        appendConfig = lib.optionalString cfg.quic.bpf /* nginx */ ''
+          quic_bpf on;
+        '' + lib.optionalString cfg.recommendedDefaults /* nginx */ ''
+          worker_processes auto;
+          worker_cpu_affinity auto;
+        '';
 
-          commonHttpConfig = /* nginx */ ''
-            error_log syslog:server=unix:/dev/log;
-          '';
+        commonHttpConfig = lib.optionalString cfg.recommendedDefaults /* nginx */ ''
+          error_log syslog:server=unix:/dev/log;
+        '' + lib.optionalString cfg.quic.enable /* nginx */''
+          quic_retry on;
+        '' + lib.optionalString cfg.recommendedZstdSettings /* nginx */ ''
+          # TODO: upstream this?
+          zstd_types application/x-nix-archive;
+        '';
 
-          virtualHosts."_" = {
-            kTLS = true;
-            reuseport = true;
-          };
-        })
+        commonServerConfig = lib.mkIf cfg.setHSTSHeader /* nginx */ ''
+          more_set_headers "Strict-Transport-Security: max-age=63072000; includeSubDomains; preload";
+        '';
 
-        {
-          commonHttpConfig = lib.mkIf cfg.recommendedZstdSettings /* nginx */ ''
-            # TODO: upstream this?
-            zstd_types application/x-nix-archive;
-          '';
+        package = lib.mkIf cfg.quic.enable pkgs.nginxQuic; # based on pkgs.nginxMainline
 
-          commonServerConfig = lib.mkIf cfg.setHSTSHeader /* nginx */ ''
-            more_set_headers "Strict-Transport-Security: max-age=63072000; includeSubDomains; preload";
-          '';
+        recommendedBrotliSettings = lib.mkIf cfg.allCompression true;
+        recommendedGzipSettings = lib.mkIf cfg.allCompression true;
+        recommendedOptimisation = lib.mkIf cfg.allCompression true;
+        recommendedProxySettings = lib.mkIf cfg.allCompression true;
+        recommendedTlsSettings = lib.mkIf cfg.allCompression true;
+        recommendedZstdSettings = lib.mkIf cfg.allCompression true;
 
-          resolver.addresses =
-            let
-              isIPv6 = addr: builtins.match ".*:.*:.*" addr != null;
-              escapeIPv6 = addr:
-                if isIPv6 addr then
-                  "[${addr}]"
-                else
-                  addr;
-            in
-            lib.optionals (cfg.resolverAddrFromNameserver && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
-          sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
+        resolver.addresses =
+          let
+            isIPv6 = addr: builtins.match ".*:.*:.*" addr != null;
+            escapeIPv6 = addr:
+              if isIPv6 addr then
+                "[${addr}]"
+              else
+                addr;
+          in
+          lib.optionals (cfg.resolverAddrFromNameserver && config.networking.nameservers != [ ]) (map escapeIPv6 config.networking.nameservers);
+        sslDhparam = lib.mkIf cfg.generateDhparams config.security.dhparams.params.nginx.path;
 
-          virtualHosts = lib.mkMerge [
-            (lib.mkIf cfg.default404Server.enable {
-              "_" = {
-                default = true;
-                forceSSL = lib.mkDefault true;
-                useACMEHost = cfg.default404Server.acmeHost;
-                extraConfig = /* nginx */ ''
-                  return 404;
-                '';
-              };
-            })
+        # NOTE: do not use mkMerge here to prevent infinite recursions
+        virtualHosts =
+          let
+            extraParameters = [
+              # net.core.somaxconn is set to 4096
+              # see https://www.nginx.com/blog/tuning-nginx/#:~:text=to%20a%20value-,greater%20than%20512,-%2C%20change%20the%20backlog
+              "backlog=1024"
 
-            (lib.mkIf cfg.tcpFastOpen (let
-              extraParameters = [
-                # net.core.somaxconn is set to 4096
-                # see https://www.nginx.com/blog/tuning-nginx/#:~:text=to%20a%20value-,greater%20than%20512,-%2C%20change%20the%20backlog
-                "backlog=1024"
+              "deferred"
+              "fastopen=256" # requires nginx to be compiled with -DTCP_FASTOPEN=23
+            ];
+          in
+          lib.mkIf (cfg.recommendedDefaults || cfg.default404Server.enable || cfg.quic.enable) {
+            "_" = {
+              kTLS = lib.mkIf cfg.recommendedDefaults true;
+              reuseport = lib.mkIf (cfg.recommendedDefaults || cfg.quic.enable) true;
 
-                "deferred"
-                "fastopen=256" # requires nginx to be compiled with -DTCP_FASTOPEN=23
-              ];
-            in {
-              "_".listen = lib.mkDefault [
+              default = lib.mkIf cfg.default404Server.enable true;
+              forceSSL = lib.mkIf cfg.default404Server.enable true;
+              useACMEHost = lib.mkIf cfg.default404Server.enable cfg.default404Server.acmeHost;
+              extraConfig = lib.mkIf cfg.default404Server.enable /* nginx */ ''
+                return 404;
+              '';
+
+              listen = lib.mkIf cfg.tcpFastOpen (lib.mkDefault [
                 { addr = "[::]"; port = 80; inherit extraParameters; }
                 { addr = "[::]"; port = 443; ssl = true; inherit extraParameters; }
-              ];
-            }))
-          ];
-        }
+              ]);
 
-        (lib.mkIf cfg.quic.enable {
-          appendConfig = lib.mkIf cfg.quic.bpf /* nginx */ ''
-            quic_bpf on;
-          '';
-
-          commonHttpConfig = lib.mkIf cfg.quic.enable /* nginx */''
-            quic_retry on;
-          '';
-
-          package = pkgs.nginxQuic; # based on pkgs.nginxMainline
-
-          virtualHosts."_" = {
-            quic = true;
-            reuseport = true;
+              quic = lib.mkIf cfg.quic.enable true;
+            };
           };
-        })
-
-        (lib.mkIf cfg.allCompression (libS.modules.mkRecursiveDefault {
-          recommendedBrotliSettings = true;
-          recommendedGzipSettings = true;
-          recommendedOptimisation = true;
-          recommendedProxySettings = true;
-          recommendedTlsSettings = true;
-          recommendedZstdSettings = true;
-        }))
-      ];
+      };
     };
 
     security.dhparams = lib.mkIf cfg.generateDhparams {
