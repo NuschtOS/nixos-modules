14ab29e8a5c99fa5694f841d389e3fba5129062b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-11-07 20:04:05 +0100

    zfs: don't always use latest kernel for zfs

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -5,10 +5,13 @@ let
 in
 {
   options = {
-    boot.zfs.recommendedDefaults = libS.mkOpinionatedOption "enable recommended ZFS settings";
+    boot.zfs = {
+      recommendedDefaults = libS.mkOpinionatedOption "enable recommended ZFS settings";
+      latestCompatibleKernel = libS.mkOpinionatedOption "use the latest ZFS compatible kernel";
+    };
   };
 
-  config = lib.mkIf (cfg.recommendedDefaults && cfg.enabled) {
+  config = lib.mkIf cfg.enabled {
     boot.kernelPackages =
       let
         ver = config.boot.zfs.package.latestCompatibleLinuxPackages.kernel.version;
@@ -16,16 +19,16 @@ in
       # 6.0 has a bug in the bind syscall and does not error correct when the port is already in use
         # https://lists.fedoraproject.org/archives/list/devel@lists.fedoraproject.org/thread/7VPNMC77YC3SI5LFYKUA4B5MTFPLTLVB/
         # https://lore.kernel.org/stable/CAFsF8vL4CGFzWMb38_XviiEgxoKX0GYup=JiUFXUOmagdk9CRg@mail.gmail.com/
-      lib.mkIf (lib.versions.majorMinor ver != "6.0") (lib.mkDefault config.boot.zfs.package.latestCompatibleLinuxPackages);
+      lib.mkIf (cfg.latestCompatibleKernel && lib.versions.majorMinor ver != "6.0") (lib.mkDefault config.boot.zfs.package.latestCompatibleLinuxPackages);
 
-    services.zfs = {
+    services.zfs = lib.mkIf cfg.recommendedDefaults {
       autoScrub.enable = true;
       trim.enable = true;
     };
 
-    virtualisation.containers.storage.settings = lib.recursiveUpdate options.virtualisation.containers.storage.settings.default {
+    virtualisation.containers.storage.settings = lib.mkIf cfg.recommendedDefaults (lib.recursiveUpdate options.virtualisation.containers.storage.settings.default {
       # fixes: Error: 'overlay' is not supported over zfs, a mount_program is required: backing file system is unsupported for this graph driver
       storage.options.mount_program = "${pkgs.fuse-overlayfs}/bin/fuse-overlayfs";
-    };
+    });
   };
 }
