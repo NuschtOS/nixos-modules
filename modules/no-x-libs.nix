6f165d72428e8f7c1c4597b4eaa085c9f333d522
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-12-26 00:07:49 +0100

    no-x-libs: add todo

--- modules/no-x-libs.nix
+++ modules/no-x-libs.nix
@@ -9,6 +9,7 @@
        # https://github.com/NixOS/nixpkgs/pull/272552
        ghostscript = super.ghostscript.override { cupsSupport = false; x11Support = false; };
        # https://github.com/NixOS/nixpkgs/pull/253263
+       # TODO: remove nixos-unstable bump
        intel-vaapi-driver = super.intel-vaapi-driver.override { enableGui = false; };
        # https://github.com/NixOS/nixpkgs/pull/213783
        vte = super.vte.override { gtkVersion = null; };
