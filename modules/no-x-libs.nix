98f5df720e06d78afb1080711d2d7ce4276b472f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-01-04 00:56:23 +0100

    no-x-libs: drop upstream modules

--- modules/no-x-libs.nix
+++ modules/no-x-libs.nix
@@ -6,11 +6,6 @@
      nixpkgs.overlays = lib.singleton (lib.const (super: {
        nginx = super.nginx.override { withImageFilter = false; };
 
-       # https://github.com/NixOS/nixpkgs/pull/272552
-       ghostscript = super.ghostscript.override { cupsSupport = false; x11Support = false; };
-       # https://github.com/NixOS/nixpkgs/pull/253263
-       # TODO: remove nixos-unstable bump
-       intel-vaapi-driver = super.intel-vaapi-driver.override { enableGui = false; };
        # https://github.com/NixOS/nixpkgs/pull/213783
        vte = super.vte.override { gtkVersion = null; };
      }));
