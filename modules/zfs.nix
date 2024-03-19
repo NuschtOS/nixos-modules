34fdf6625852d421407e3654472e81de4ba2f6ca
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-03-19 23:13:45 +0100

    zfs: remove kernel 6.0 workaround

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -12,14 +12,7 @@ in
   };
 
   config = lib.mkIf cfg.enabled {
-    boot.kernelPackages =
-      let
-        ver = config.boot.zfs.package.latestCompatibleLinuxPackages.kernel.version;
-      in
-      # 6.0 has a bug in the bind syscall and does not error correct when the port is already in use
-        # https://lists.fedoraproject.org/archives/list/devel@lists.fedoraproject.org/thread/7VPNMC77YC3SI5LFYKUA4B5MTFPLTLVB/
-        # https://lore.kernel.org/stable/CAFsF8vL4CGFzWMb38_XviiEgxoKX0GYup=JiUFXUOmagdk9CRg@mail.gmail.com/
-      lib.mkIf (cfg.latestCompatibleKernel && lib.versions.majorMinor ver != "6.0") (lib.mkDefault config.boot.zfs.package.latestCompatibleLinuxPackages);
+    boot.kernelPackages = lib.mkIf cfg.latestCompatibleKernel config.boot.zfs.package.latestCompatibleLinuxPackages;
 
     services.zfs = lib.mkIf cfg.recommendedDefaults {
       autoScrub.enable = true;
