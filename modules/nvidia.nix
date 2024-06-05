d30d1bc2650f35e79e2755af5988b5addf5fb308
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-06-06 00:56:21 +0200

    nvidia: fix EGL env, validate that json files exist
    otherwise even SDDM doesn't start

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.hardware;
@@ -12,7 +12,7 @@ in
     environment.sessionVariables = {
       # source https://github.com/elFarto/nvidia-vaapi-driver#firefox
       LIBVA_DRIVER_NAME = "nvidia";
-      __EGL_VENDOR_LIBRARY_FILENAMES = "nvidia";
+      __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/egl/egl_external_platform.d/10_nvidia_wayland.json";
 
       # source https://discourse.nixos.org/t/nvk-error-when-using-prop-nvidia-drivers/43300/4
       VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
@@ -26,5 +26,17 @@ in
     programs.firefox.hardwareAcceleration = true;
 
     services.xserver.videoDrivers = [ "nvidia" ];
+
+    system.checks = let
+      openglDriver = config.systemd.tmpfiles.settings.opengl."/run/opengl-driver"."L+".argument;
+    in [
+      (pkgs.runCommand "check-that-nvidia-driver-json-files-exist" {} /* bash */ ''
+        set -eoux pipefail
+
+        # the cut drops /run/opengl-driver/
+        [[ -e ${openglDriver}/$(echo ${config.environment.sessionVariables.__EGL_VENDOR_LIBRARY_FILENAMES} | cut -d'/' -f4-) ]]
+        [[ -e ${openglDriver}/$(echo ${config.environment.sessionVariables.VK_DRIVER_FILES} | cut -d'/' -f4-) ]]
+      '')
+    ];
   };
 }
