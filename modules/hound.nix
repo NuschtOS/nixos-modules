227a494254e6e28cba9f4230dd717295c461e1b0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-06-26 21:36:49 +0200

    hound: add repos convenience option

---
+++ modules/hound.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.services.hound;
+in
+{
+  options = {
+    services.hound = {
+      repos = lib.mkOption {
+        type = with lib.types; listOf str;
+        default = [];
+        example = [ "https://github.com/NuschtOS/nixos-modules.git" ];
+        description = ''
+          A list of repos which should be fetched from their default branch. The display name is derived using builtins.baseNameOf and .git is stripped
+        '';
+      };
+    };
+  };
+
+  config = lib.mkIf (cfg.enable && cfg.repos != [ ]) {
+    services.hound.settings = {
+      vcs-config.git.detect-ref = true;
+      repos = lib.listToAttrs (map (url: lib.nameValuePair
+        (lib.removeSuffix ".git" (builtins.baseNameOf url))
+        { inherit url; }
+      ) cfg.repos);
+    };
+  };
+}
