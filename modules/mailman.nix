f8e5d3dc60185b639875305fbb87567b42e78ca8
Marcel <me@m4rc3l.de>
2024-08-04 22:45:54 +0200

    mailman: init

---
+++ modules/mailman.nix
@@ -0,0 +1,74 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.services.mailman;
+in
+{
+  options = {
+    services.mailman = {
+      enablePostgres = lib.mkEnableOption "configure postgres as a database backend";
+
+      openidConnect = {
+        enable = lib.mkEnableOption "login only via OpenID Connect";
+        clientSecretFile = lib.mkOption {
+          type = lib.types.str;
+          description = "Path of the file containing the client id";
+        };
+      };
+    };
+  };
+
+  config.environment.etc = lib.mkIf cfg.openidConnect.enable {
+    "mailman3/settings.py".text = lib.mkAfter /* python */ ''
+      INSTALLED_APPS.append('allauth.socialaccount.providers.openid_connect')
+
+      with open('${cfg.openidConnect.clientSecretFile}') as f:
+        SOCIALACCOUNT_PROVIDERS = {
+          "openid_connect": {
+            "APPS": [{
+              "provider_id": "dex",
+              "name": "${config.services.portunus.webDomain}",
+              "client_id": "mailman",
+              "secret": f.read(),
+              "settings": {
+                "server_url": "${config.services.dex.settings.issuer}",
+              },
+            }],
+          }
+        }
+    '';
+  };
+
+  config.services.mailman = lib.mkIf cfg.enablePostgres {
+    settings.database = {
+      class = "mailman.database.postgresql.PostgreSQLDatabase";
+      url = "postgres://mailman@/mailman?host=/run/postgresql";
+    };
+    webSettings = {
+      DATABASES.default = {
+        ENGINE = "django.db.backends.postgresql";
+        NAME = "mailman_web";
+        USER = "mailman_web";
+      };
+    };
+  };
+
+  config.services.postgresql = lib.mkIf cfg.enablePostgres {
+    ensureDatabases = [ "mailman" "mailman_web" ];
+    ensureUsers = [ {
+      name = "mailman";
+      ensureDBOwnership = true;
+    } {
+      name = "mailman_web";
+      ensureDBOwnership = true;
+    } ];
+  };
+
+  config.services.portunus.dex = lib.mkIf cfg.openidConnect.enable {
+    enable = true;
+    oidcClients = [{
+      callbackURL = "https://${lib.elemAt cfg.webHosts 0}";
+      id = "mailman";
+    }];
+  };
+}
