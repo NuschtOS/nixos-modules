7ded0e8342ac83e368dff9e16d3cdc1f0d7db87d
Sandro <sandro.jaeckel@gmail.com>
2024-08-13 11:25:51 +0200

    Drop no longer required override

--- modules/no-x-libs.nix
+++ modules/no-x-libs.nix
@@ -3,8 +3,7 @@
 # this module extends the environment.noXlibs setting with yet to merge and upstream overwrites
 {
   config = lib.mkIf config.environment.noXlibs {
-    nixpkgs.overlays = lib.singleton (lib.const (super: {
-      nginx = super.nginx.override { withImageFilter = false; };
-    }));
+    # nixpkgs.overlays = lib.singleton (lib.const (super: {
+    # }));
   };
 }
