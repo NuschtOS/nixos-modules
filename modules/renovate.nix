88f337a15a6715965022a5c6b65704baa469fb38
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-11-04 00:22:51 +0100

    renovate: fix condition on nixos-unstable

--- modules/renovate.nix
+++ modules/renovate.nix
@@ -1,31 +1,22 @@
-{ config, lib, libS, ... }:
+{ config, lib, libS, options, ... }:
 
 let
   cfg = config.services.renovate;
 in
 {
-  options.services.renovate = lib.optionalAttrs (!lib.versionAtLeast lib.version "24.11") {
-    # TODO: clean up when updating to 24.11
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      internal = !(lib.versionAtLeast lib.version "24.11");
-    };
-    settings = lib.mkOption {
-      type = lib.types.freeformSetting;
-      internal = !(lib.versionAtLeast lib.version "24.11");
-    };
-  } // {
+  options.services.renovate = {
     recommendedDefaults = libS.mkOpinionatedOption "set recommended default settings";
   };
 
-  config = lib.mkIf cfg.enable {
-    services.renovate.settings = {
-      cachePrivatePackages = true;
-      configMigration = true;
-      optimizeForDisabled = true;
-      persistRepoData = true;
-      repositoryCache = "enabled";
+  config = lib.mkIf (cfg?enable && cfg.enable) {
+    services = lib.optionalAttrs (options.services.renovate?settings) {
+      renovate.settings = {
+        cachePrivatePackages = true;
+        configMigration = true;
+        optimizeForDisabled = true;
+        persistRepoData = true;
+        repositoryCache = "enabled";
+      };
     };
   };
 }
