27333c1acbff35baf83bcb915cc26e896cdfdbde
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-03-05 01:24:54 +0100

    mastodon: switch to more up to date bird-ui

--- modules/mastodon.nix
+++ modules/mastodon.nix
@@ -46,40 +46,54 @@ in
   config = {
     nixpkgs.overlays = lib.mkIf cfg.enableBirdUITheme [
       (final: prev: {
-        mastodon = with prev; mastodon.overrideAttrs (_: let
+        mastodon = (prev.mastodon.override {
+          patches = [
+            # redone based on https://codeberg.org/rheinneckar.social/nixos-config/src/branch/main/patches/mastodon-bird-ui.patch
+            ./mastodon-bird-ui.patch
+          ];
+        }).overrideAttrs (oldAttrs: let
           src = pkgs.applyPatches {
-            src = fetchFromGitHub {
-              owner = "mstdn";
-              repo = "Bird-UI-Theme-Admins";
-              rev = "2f9921db746593f393c13f9b79e5b4c2e19b03bd";
-              hash = "sha256-+7FUm5GNXRWyS9Oiow6kwX+pWh11wO3stm5iOTY3sYY=";
+            src = final.fetchFromGitHub {
+              owner = "ronilaukkarinen";
+              repo = "mastodon-bird-ui";
+              tag = "2.1.1";
+              hash = "sha256-WEw9wE+iBCLDDTZjFoDJ3EwKTY92+LyJyDqCIoVXhzk=";
             };
 
-            patches = [
-              # fix compose box background
-              (fetchpatch {
-                url = "https://github.com/mstdn/Bird-UI-Theme-Admins/commit/d5a07d653680fba0ad8dd941405e2d0272ff9cd1.patch";
-                hash = "sha256-1gnQNCSSuTE/pkPCf49lJQbmeLAbaiPD9u/q8KiFvlU=";
-              })
-            ];
+            # based on:
+            # https://github.com/ronilaukkarinen/mastodon-bird-ui#make-mastodon-bird-ui-as-optional-by-integrating-it-as-site-theme-in-settings-for-all-users
+            postPatch = ''
+              substituteInPlace layout-single-column.css layout-multiple-columns.css \
+                --replace-fail theme-contrast theme-mastodon-bird-ui-contrast \
+                --replace-fail theme-mastodon-light theme-mastodon-bird-ui-light
+
+              mkdir mastodon-bird-ui
+              mv layout-single-column.css mastodon-bird-ui/layout-single-column.scss
+              mv layout-multiple-columns.css mastodon-bird-ui/layout-multiple-columns.scss
+
+              echo -e "@import 'contrast/variables';
+              @import 'application';
+              @import 'contrast/diff';
+              @import 'mastodon-bird-ui/layout-single-column.scss';
+              @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-contrast.scss
+              echo -e "@import 'mastodon-light/variables';
+              @import 'application';
+              @import 'mastodon-light/diff';
+              @import 'mastodon-bird-ui/layout-single-column.scss';
+              @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-light.scss
+              echo -e "@import 'application';
+              @import 'mastodon-bird-ui/layout-single-column.scss';
+              @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-dark.scss
+            '';
           };
         in {
-          mastodonModules = mastodon.mastodonModules.overrideAttrs (oldAttrs: {
+          mastodonModules = oldAttrs.mastodonModules.overrideAttrs (oldAttrs: {
             pname = "mastodon-birdui-theme";
 
-            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-              rsync
-              xorg.lndir
-            ];
-
-            postPatch = ''
-              rsync -r ${src}/mastodon/ .
+            postPatch = oldAttrs.postPatch or "" + ''
+              cp -r ${src}/*.scss ${src}/mastodon-bird-ui/ app/javascript/styles/
             '';
           });
-
-          postBuild = ''
-            cp ${src}/mastodon/config/themes.yml config/themes.yml
-          '';
         });
       })
     ];
