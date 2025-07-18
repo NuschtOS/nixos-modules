1e0d78dddb4d1b77eda8205e96dff4c681eb85e3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-07-18 15:45:36 +0200

    renovate: clean up hackery

--- modules/renovate.nix
+++ modules/renovate.nix
@@ -1,22 +1,22 @@
-{ config, lib, libS, options, ... }:
+{ config, lib, libS, ... }:
 
 let
   cfg = config.services.renovate;
 in
 {
-  options.services.renovate = {
-    recommendedDefaults = libS.mkOpinionatedOption "set recommended default settings";
+  options = {
+    services.renovate = {
+      recommendedDefaults = libS.mkOpinionatedOption "set recommended default settings";
+    };
   };
 
-  config = lib.mkIf (cfg?enable && cfg.enable) {
-    services = lib.optionalAttrs (options.services.renovate?settings) {
-      renovate.settings = {
-        cachePrivatePackages = true;
-        configMigration = true;
-        optimizeForDisabled = true;
-        persistRepoData = true;
-        repositoryCache = "enabled";
-      };
+  config = lib.mkIf cfg.enable {
+    services.renovate.settings = {
+      cachePrivatePackages = true;
+      configMigration = true;
+      optimizeForDisabled = true;
+      persistRepoData = true;
+      repositoryCache = "enabled";
     };
   };
 }
