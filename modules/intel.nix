9b3920414709270a69fd58ac3b17fd27ac55ac21
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-07-22 23:13:43 +0200

    intel: drop

--- modules/intel.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }:
-
-{
-  imports = [
-    # TODO: drop with nixos 25.05
-    (lib.mkRemovedOptionModule ["hardware" "intelGPU"] "Please use hardware.intelgpu from nixos-hardware instead.")
-  ];
-}
