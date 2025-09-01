4bac595d508129bddee1af0190b456ad8bd93937
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-09-01 15:18:48 +0200

    strace: use merged patch

--- modules/strace.nix
+++ modules/strace.nix
@@ -14,18 +14,13 @@ in
     environment.systemPackages = [
       (pkgs.strace.overrideAttrs ({ patches ? [ ], version, ... }: {
         patches = patches ++ [
-          (let
-            patchVersion = if lib.versionAtLeast version "6.16" then "6.16" else "6.3";
-          in pkgs.fetchpatch {
-            url = if patchVersion == "6.16" then
-              "https://raw.githubusercontent.com/Ma27/strace-with-colors/strace-6.16/strace-with-colors.patch"
-            else
-              "https://github.com/xfgusta/strace-with-colors/raw/v${patchVersion}-1/strace-with-colors.patch";
-            name = "strace-with-colors-${patchVersion}.patch";
+          (pkgs.fetchpatch {
+            url = "https://github.com/xfgusta/strace-with-colors/raw/v${version}-1/strace-with-colors.patch";
+            name = "strace-with-colors-${version}.patch";
             hash = {
               "6.16" = "sha256-Uw4lOKuEwT6kTwLZYuTqlq64wBHDt5kL5JwV7hdiBNg=";
               "6.3" = "sha256-gcQldGsRgvGnrDX0zqcLTpEpchNEbCUFdKyii0wetEI=";
-            }.${patchVersion} or (throw "nixos-modules.strace: do not know a patch for strace version ${version}");
+            }.${version} or (throw "nixos-modules.strace: does not know a patch for strace version ${version}");
           })
         ];
       }))
