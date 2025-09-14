34bd450991f8d73d81e1baf55e6f9fd456f786c6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-09-14 20:10:34 +0200

    chrony: add

---
+++ modules/chrony.nix
@@ -0,0 +1,50 @@
+{ config, lib, libS, ... }:
+
+let
+  cfg = config.services.chrony;
+in
+{
+  options = {
+    networking.recommendedTimeServers = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to configure recommended stratum 1 time servers.";
+    };
+
+    services.chrony = {
+      configureServer = libS.mkOpinionatedOption "configure recommended server settings";
+
+      openFirewall = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Whether to open the firewall for the ntp protocol of chrony.";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    networking = {
+      firewall = lib.mkIf cfg.openFirewall {
+        allowedTCPPorts = [ 123 ];
+        allowedUDPPorts = [ 123 ];
+      };
+      recommendedTimeServers = lib.mkIf cfg.configureServer true;
+      timeServers = lib.mkIf config.networking.recommendedTimeServers [
+        "ntp0.fau.de"
+        "ntp1.hetzner.de"
+        "ntps1-0.eecsit.tu-berlin.de"
+        "ntps1-0.fh-mainz.de"
+        "ptbtime1.ptb.de"
+        "rustime01.rus.uni-stuttgart.de"
+        "zeit.fu-berlin.de"
+      ];
+    };
+
+    services.chrony = {
+      openFirewall = true;
+      extraConfig = lib.mkIf cfg.configureServer ''
+        allow
+      '';
+    };
+  };
+}
