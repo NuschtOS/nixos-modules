b4b4605e116cee08109cba7a9dbda4d57ad82808
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-10-24 01:13:59 +0200

    strace: fix strace 6.17

--- modules/strace.nix
+++ modules/strace.nix
@@ -14,13 +14,15 @@ in
     environment.systemPackages = [
       (pkgs.strace.overrideAttrs ({ patches ? [ ], version, ... }: {
         patches = patches ++ [
-          (pkgs.fetchpatch {
-            url = "https://github.com/xfgusta/strace-with-colors/raw/v${version}-1/strace-with-colors.patch";
+          (let
+            patchVersion = if lib.versionAtLeast version "6.16" then "6.16" else "6.3";
+          in pkgs.fetchpatch {
+            url = "https://github.com/xfgusta/strace-with-colors/raw/v${patchVersion}-1/strace-with-colors.patch";
             name = "strace-with-colors-${version}.patch";
             hash = {
               "6.16" = "sha256-Uw4lOKuEwT6kTwLZYuTqlq64wBHDt5kL5JwV7hdiBNg=";
               "6.3" = "sha256-gcQldGsRgvGnrDX0zqcLTpEpchNEbCUFdKyii0wetEI=";
-            }.${version} or (throw "nixos-modules.strace: does not know a patch for strace version ${version}");
+            }.${patchVersion} or (throw "nixos-modules.strace: does not know a patch for strace version ${version}");
           })
         ];
       }))
